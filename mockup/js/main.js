3b5d49f8af3ffd77ddfff3b7ab67514af8d44842
glanonim <6484926+glanonim@users.noreply.github.com>
2019-12-23 10:41:54 +0100

    Complete different data on line chart view

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -34,6 +34,17 @@
       $("#hideSidePanel").toggle();
       $("#showSidePanel").toggle();
     });
+    // show and hide side panel to select line chart data
+    $("#hideLinePanel").click(function () {
+      $("#chartLine").toggle();
+      $("#hideLinePanel").toggle();
+      $("#showLinePanel").toggle();
+    });
+    $("#showLinePanel").click(function () {
+      $("#chartLine").toggle();
+      $("#hideLinePanel").toggle();
+      $("#showLinePanel").toggle();
+    });
 
     // change map category
     // display per citizen, color: primary
@@ -1697,44 +1708,211 @@
   // line chart
   am4core.ready(function () {
 
-    // Themes begin
-    am4core.useTheme(am4themes_animated);
-    // Themes end
+   
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
-    var chart = am4core.create("lineChart", am4charts.XYChart);
+// Create chart instance
+var chart = am4core.create("linediv", am4charts.XYChart);
 
-    var data = [];
-    var value = 50;
-    for (let i = 0; i < 300; i++) {
-      let date = new Date();
-      date.setHours(0, 0, 0, 0);
-      date.setDate(i);
-      value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({ date: date, value: value });
-    }
+// Add data
+chart.data = [{
+  "year": "1995",
+  "cars": 1567,
+  "motorcycles": 683,
+  "bicycles": 146
+}, {
+  "year": "1996",
+  "cars": 1617,
+  "motorcycles": 691,
+  "bicycles": 138
+}, {
+  "year": "1997",
+  "cars": 1630,
+  "motorcycles": 642,
+  "bicycles": 127
+}, {
+  "year": "1998",
+  "cars": 1660,
+  "motorcycles": 699,
+  "bicycles": 105
+}, {
+  "year": "1999",
+  "cars": 1683,
+  "motorcycles": 721,
+  "bicycles": 109
+}, {
+  "year": "2000",
+  "cars": 1691,
+  "motorcycles": 737,
+  "bicycles": 112
+}, {
+  "year": "2001",
+  "cars": 1298,
+  "motorcycles": 680,
+  "bicycles": 101
+}, {
+  "year": "2002",
+  "cars": 1275,
+  "motorcycles": 664,
+  "bicycles": 97
+}, {
+  "year": "2003",
+  "cars": 1246,
+  "motorcycles": 648,
+  "bicycles": 93
+}, {
+  "year": "2004",
+  "cars": 1218,
+  "motorcycles": 637,
+  "bicycles": 101
+}, {
+  "year": "2005",
+  "cars": 1213,
+  "motorcycles": 633,
+  "bicycles": 87
+}, {
+  "year": "2006",
+  "cars": 1199,
+  "motorcycles": 621,
+  "bicycles": 79
+}, {
+  "year": "2007",
+  "cars": 1110,
+  "motorcycles": 210,
+  "bicycles": 81
+}, {
+  "year": "2008",
+  "cars": 1165,
+  "motorcycles": 232,
+  "bicycles": 75
+}, {
+  "year": "2009",
+  "cars": 1145,
+  "motorcycles": 219,
+  "bicycles": 88
+}, {
+  "year": "2010",
+  "cars": 1163,
+  "motorcycles": 201,
+  "bicycles": 82
+}, {
+  "year": "2011",
+  "cars": 1180,
+  "motorcycles": 285,
+  "bicycles": 87
+}, {
+  "year": "2012",
+  "cars": 1159,
+  "motorcycles": 277,
+  "bicycles": 71
+}];
 
-    chart.data = data;
+// Create axes
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "year";
+categoryAxis.title.text = "Year";
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.renderer.minGridDistance = 20;
 
-    // Create axes
-    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.minGridDistance = 60;
+categoryAxis.startLocation = 0.5;
+categoryAxis.endLocation = 0.5;
 
-    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
-    // Create series
-    var series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.valueY = "value";
-    series.dataFields.dateX = "date";
-    series.tooltipText = "{value}"
+var  valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.title.text = "Percent";
+valueAxis.calculateTotals = true;
+valueAxis.min = 0;
+valueAxis.max = 100;
+valueAxis.strictMinMax = true;
+valueAxis.renderer.labels.template.adapter.add("text", function(text) {
+  return text + "%";
+});
 
-    series.tooltip.pointerOrientation = "vertical";
+// Create series
+var series = chart.series.push(new am4charts.LineSeries());
+series.dataFields.valueY = "cars";
+series.dataFields.valueYShow = "totalPercent";
+series.dataFields.categoryX = "year";
+series.name = "Cars";
 
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.snapToSeries = series;
-    chart.cursor.xAxis = dateAxis;
+series.tooltipHTML = "<img src='https://www.amcharts.com/lib/3/images/car.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>{valueY.value}</b></span>";
 
-    //chart.scrollbarY = new am4core.Scrollbar();
-    chart.scrollbarX = new am4core.Scrollbar();
+series.tooltip.getFillFromObject = false;
+series.tooltip.background.fill = am4core.color("#FFF");
+
+series.tooltip.getStrokeFromObject = true;
+series.tooltip.background.strokeWidth = 3;
+
+series.fillOpacity = 0.5;
+series.stacked = true;
+
+// static
+series.legendSettings.labelText = "Cars total:";
+series.legendSettings.valueText = "{valueY.close}";
+
+// hovering
+series.legendSettings.itemLabelText = "Cars:";
+series.legendSettings.itemValueText = "{valueY}";
+
+var series2 = chart.series.push(new am4charts.LineSeries());
+series2.dataFields.valueY = "motorcycles";
+series2.dataFields.valueYShow = "totalPercent";
+series2.dataFields.categoryX = "year";
+series2.name = "Motorcycles";
+
+series2.tooltipHTML = "<img src='https://www.amcharts.com/lib/3/images/motorcycle.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>{valueY.value}</b></span>";
+
+series2.tooltip.getFillFromObject = false;
+series2.tooltip.background.fill = am4core.color("#FFF");
+
+series2.tooltip.getStrokeFromObject = true;
+series2.tooltip.background.strokeWidth = 3;
+
+series2.fillOpacity = 0.5;
+series2.stacked = true;
+
+// static
+series2.legendSettings.labelText = "Motorcycles total:";
+series2.legendSettings.valueText = "{valueY.close}";
+
+// hovering
+series2.legendSettings.itemLabelText = "Motorcycles:";
+series2.legendSettings.itemValueText = "{valueY}";
+
+var series3 = chart.series.push(new am4charts.LineSeries());
+series3.dataFields.valueY = "bicycles";
+series3.dataFields.valueYShow = "totalPercent";
+series3.dataFields.categoryX = "year";
+series3.name = "Bicycles";
+series3.tooltipText = "{name}: [bold]{valueY}[/]";
+
+series3.tooltipHTML = "<img src='https://www.amcharts.com/lib/3/images/bicycle.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'><b>{valueY.value}</b></span>";
+
+series3.tooltip.getFillFromObject = false;
+series3.tooltip.background.fill = am4core.color("#FFF");
+
+series3.tooltip.getStrokeFromObject = true;
+series3.tooltip.background.strokeWidth = 3;
+
+series3.fillOpacity = 0.5;
+series3.stacked = true;
+
+// static
+series3.legendSettings.labelText = "Bicycles total:";
+series3.legendSettings.valueText = "{valueY.close}";
+
+// hovering
+series3.legendSettings.itemLabelText = "Bicycles:";
+series3.legendSettings.itemValueText = "{valueY}";
+
+// Add cursor
+chart.cursor = new am4charts.XYCursor();
+
+// add legend
+chart.legend = new am4charts.Legend();
+
 
   }); // end am4core.ready()
 
@@ -1764,11 +1942,11 @@
     // Themes end
 
     /* Create chart instance */
-    var chart = am4core.create("radar-transport", am4charts.RadarChart);
+    var chart = am4core.create("radar-t1", am4charts.RadarChart);
     /* Create chart instance */
-    var chartEnergy = am4core.create("radar-energy", am4charts.RadarChart);
+    var chartEnergy = am4core.create("radar-t2", am4charts.RadarChart);
     /* Create chart instance */
-    var chartEnergyCit = am4core.create("radar-energy-cit", am4charts.RadarChart);
+    var chartEnergyCit = am4core.create("radar-t3", am4charts.RadarChart);
 
     /* Add data */
     chart.data = [{
